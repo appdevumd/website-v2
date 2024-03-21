860f196e8a8ce7ab988614b9ba60073fa2c6f209
Atheesh Thirumalairajan <github-commits@atheesh.org>
2024-03-20 18:17:06 -0700

    Event Bar Visibility Actions saved to LocalStorage

--- src/components/WebEventsBar.tsx
+++ src/components/WebEventsBar.tsx
@@ -1,5 +1,6 @@
 import { Close } from "@mui/icons-material";
 import { Box, IconButton, Link, Typography } from "@mui/material";
+import React from "react";
 
 export interface WebEvent {
     title: string,
@@ -7,12 +8,28 @@ export interface WebEvent {
 }
 
 export default function WebEventsBar(props: { events: WebEvent[] }) {
+    const [eventsVisible, setEventsVisible] = React.useState(false);
+
+    const handleEventsClose = () => {
+        window.localStorage.setItem("events-visible", "false")
+        setEventsVisible(false)
+    }
+
+    React.useEffect(() => {
+        if (window.localStorage.getItem("events-visible") == null)
+            window.localStorage.setItem("events-visible", "true")
+        
+        /* Set Events Visible */
+        let visibility = window.localStorage.getItem("events-visible")
+        setEventsVisible(visibility != null ? JSON.parse(visibility) : true)
+    }, []);
+
     return (
         <Box sx={{
             background: "#16F4D0",
             color: "#000000",
             fontWeight: 600,
-            display: 'flex',
+            display: (eventsVisible) ? 'flex' : 'none',
             justifyContent: 'center',
             alignItems: 'center',
             height: '30px'
@@ -29,7 +46,14 @@ export default function WebEventsBar(props: { events: WebEvent[] }) {
                     ))
                 }
             </Box>
-            <IconButton sx={{ color: 'inherit', width: '25px', height: '25px', marginRight: '5px' }}><Close sx={{ color: 'inherit' }} /></IconButton>
+            <IconButton 
+                onClick={() => { setEventsVisible(false) }}
+                sx={{ 
+                    color: 'inherit', 
+                    width: '25px', 
+                    height: '25px', 
+                    marginRight: '5px' 
+            }}><Close sx={{ color: 'inherit' }} /></IconButton>
         </Box>
     );
 }
