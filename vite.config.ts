af669ce9cc2b7973fc1c6a6bd335d6d77d42975d
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2024-03-27 15:29:17 -0400

    hardcoded port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// Used vitest's wrapper of vite's defineConfig
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Hardcode port 3000
+  server: {
+    port: 3000
+  },
+});
