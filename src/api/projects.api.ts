e0d27e4d652b58c8aa9dd28cedee80460c46309b
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2024-04-14 14:02:31 -0400

    hotfix api error

--- src/api/projects.api.ts
+++ src/api/projects.api.ts
@@ -1,39 +1,70 @@
 import api from "./axios.config";
 
 async function getAll() {
-  const response = await api.get('/projects');
-
-  if (response.status === 200) {
+  try {
+    const response = await api.get('/projects');
     return response.data;
+  } catch (e) {
+    // return filter data on panic for now
+    return [
+      {
+        id: "1",
+        name: "Space Safety Visualizer",
+        organization: "Amazon",
+        description: "The Fall 2023 Amazon project was a low Earth orbit satellite visualization system. This was designed for Project Kuiper as a way to visualize satellite collision risk to non-technical stakeholders at Amazon.",
+        members: ["Samai Patel", "Ishan Revankar", "Nitish Vobilisetti", "Neil Hajela", "Hadijat Oke", "Nand Vinchhi"],
+        cover: "null",
+      },
+      {
+        id: "2",
+        name: "Space Safety Visualizer",
+        organization: "Amazon",
+        description: "The Fall 2023 Amazon project was a low Earth orbit satellite visualization system. This was designed for Project Kuiper as a way to visualize satellite collision risk to non-technical stakeholders at Amazon.",
+        members: ["Samai Patel", "Ishan Revankar", "Nitish Vobilisetti", "Neil Hajela", "Hadijat Oke", "Nand Vinchhi"],
+        cover: "null",
+      },
+      {
+        id: "3",
+        name: "Space Safety Visualizer",
+        organization: "Amazon",
+        description: "The Fall 2023 Amazon project was a low Earth orbit satellite visualization system. This was designed for Project Kuiper as a way to visualize satellite collision risk to non-technical stakeholders at Amazon.",
+        members: ["Samai Patel", "Ishan Revankar", "Nitish Vobilisetti", "Neil Hajela", "Hadijat Oke", "Nand Vinchhi"],
+        cover: "null",
+      },
+      {
+        id: "4",
+        name: "Space Safety Visualizer",
+        organization: "Amazon",
+        description: "The Fall 2023 Amazon project was a low Earth orbit satellite visualization system. This was designed for Project Kuiper as a way to visualize satellite collision risk to non-technical stakeholders at Amazon.",
+        members: ["Samai Patel", "Ishan Revankar", "Nitish Vobilisetti", "Neil Hajela", "Hadijat Oke", "Nand Vinchhi"],
+        cover: "null",
+      },
+      {
+        id: "5",
+        name: "Space Safety Visualizer",
+        organization: "Amazon",
+        description: "The Fall 2023 Amazon project was a low Earth orbit satellite visualization system. This was designed for Project Kuiper as a way to visualize satellite collision risk to non-technical stakeholders at Amazon.",
+        members: ["Samai Patel", "Ishan Revankar", "Nitish Vobilisetti", "Neil Hajela", "Hadijat Oke", "Nand Vinchhi"],
+        cover: "null",
+      },
+      {
+        id: "6",
+        name: "Space Safety Visualizer",
+        organization: "Amazon",
+        description: "The Fall 2023 Amazon project was a low Earth orbit satellite visualization system. This was designed for Project Kuiper as a way to visualize satellite collision risk to non-technical stakeholders at Amazon.",
+        members: ["Samai Patel", "Ishan Revankar", "Nitish Vobilisetti", "Neil Hajela", "Hadijat Oke", "Nand Vinchhi"],
+        cover: "null",
+      },
+      {
+        id: "7",
+        name: "Space Safety Visualizer",
+        organization: "Amazon",
+        description: "The Fall 2023 Amazon project was a low Earth orbit satellite visualization system. This was designed for Project Kuiper as a way to visualize satellite collision risk to non-technical stakeholders at Amazon.",
+        members: ["Samai Patel", "Ishan Revankar", "Nitish Vobilisetti", "Neil Hajela", "Hadijat Oke", "Nand Vinchhi"],
+        cover: "null",
+      }
+    ];
   }
-
-  // return filter data on panic for now
-  return [
-    {
-      id: "1",
-      name: "Space Safety Visualizer",
-      organization: "Amazon",
-      description: "The Fall 2023 Amazon project was a low Earth orbit satellite visualization system. This was designed for Project Kuiper as a way to visualize satellite collision risk to non-technical stakeholders at Amazon.",
-      members: ["Samai Patel", "Ishan Revankar", "Nitish Vobilisetti", "Neil Hajela", "Hadijat Oke", "Nand Vinchhi"],
-      cover: "null",
-    },
-    {
-      id: "2",
-      name: "Space Safety Visualizer",
-      organization: "Amazon",
-      description: "The Fall 2023 Amazon project was a low Earth orbit satellite visualization system. This was designed for Project Kuiper as a way to visualize satellite collision risk to non-technical stakeholders at Amazon.",
-      members: ["Samai Patel", "Ishan Revankar", "Nitish Vobilisetti", "Neil Hajela", "Hadijat Oke", "Nand Vinchhi"],
-      cover: "null",
-    },
-    {
-      id: "3",
-      name: "Space Safety Visualizer",
-      organization: "Amazon",
-      description: "The Fall 2023 Amazon project was a low Earth orbit satellite visualization system. This was designed for Project Kuiper as a way to visualize satellite collision risk to non-technical stakeholders at Amazon.",
-      members: ["Samai Patel", "Ishan Revankar", "Nitish Vobilisetti", "Neil Hajela", "Hadijat Oke", "Nand Vinchhi"],
-      cover: "null",
-    }
-  ];
 }
 
 const ProjectAPI = {
