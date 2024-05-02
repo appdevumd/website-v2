30ed66783a07426b8c0527994acbbf0e1ed8be7d
Aarya Dubhashi <aarya.dubhashi@gmail.com>
2024-05-02 00:24:13 -0400

    changed removed image icons and used material ui, added copyright

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import {Grid, Stack} from "@mui/material"
-import instagram from "./SocialMediaIcons/icons8-instagram-24.png"
-import facebook from "./SocialMediaIcons/icons8-facebook-384.png"
-import twitter from "./SocialMediaIcons/icons8-twitterx-384.png"
+import { Instagram } from "@mui/icons-material";
+import { Facebook } from "@mui/icons-material";
+import { X } from "@mui/icons-material";
+import { YouTube } from "@mui/icons-material";
 
 const Container = styled.div`
     padding: 20px 60px;
@@ -21,55 +22,70 @@ const Wrapper = styled.div`
 
 
 function Footer (){
+    const today = new Date()
+    const year = today.getFullYear();
     return (
         <div>
-            <hr style={{width:"90%"}}></hr>
             <Container>
-            <Wrapper>
-                <Grid container>
+                <hr style={{width:"100%"}}></hr>
+                <Wrapper>
+                    <Grid container>
 
-                    {/* ROW 1 /*/}
-                    <Grid container item spacing={26}>
-                        <Grid item> 
-                            <p style={{fontWeight:"bold", fontSize:"18px"}}>Students</p>
-                            <Stack spacing={0}>
-                                <p style={{fontSize:"13px"}}>Enrollment</p>
-                                <p style={{fontSize:"13px"}}>Student Benefits</p>
-                                <p style={{fontSize:"13px"}}>Upcoming Events</p>
-                            </Stack>
-                        </Grid>
-                        <Grid item>
-                            <p style= {{fontWeight:"bold", fontSize:"18px"}}>Sponsors</p>
-                            <Stack spacing={0}>
-                                <p style={{fontSize:"13px"}}>About Us</p>
-                                <p style={{fontSize:"13px"}}>Sponsor Benefits</p>
-                                <p style={{fontSize:"13px"}}>Current Projects</p>
-                            </Stack>
+                        {/* ROW 1 /*/}
+                        <Grid container item spacing={26}>
+                            <Grid item> 
+                                <p style={{fontWeight:"bold", fontSize:"18px"}}>Students</p>
+                                <Stack spacing={0}>
+                                    <p style={{fontSize:"13px"}}>Enrollment</p>
+                                    <p style={{fontSize:"13px"}}>Student Benefits</p>
+                                    <p style={{fontSize:"13px"}}>Upcoming Events</p>
+                                </Stack>
+                            </Grid>
+                            <Grid item>
+                                <p style= {{fontWeight:"bold", fontSize:"18px"}}>Sponsors</p>
+                                <Stack spacing={0}>
+                                    <p style={{fontSize:"13px"}}>Contact Us</p>
+                                    <p style={{fontSize:"13px"}}>About Us</p>
+                                    <p style={{fontSize:"13px"}}>Sponsor Benefits</p>
+                                    <p style={{fontSize:"13px"}}>Current Projects</p>
+                                </Stack>
 
+                            </Grid>
+                            <Grid item>
+                                <p style= {{fontWeight:"bold", fontSize:"18px"}}>Members</p>
+                                <Stack spacing={0}>
+                                    <p style={{fontSize:"13px"}}>Member Events</p>
+                                    <p style={{fontSize:"13px"}}>Our Team</p>
+                                    <p style={{fontSize:"13px"}}>Highlights</p>
+                                </Stack>
+                            </Grid>
+                            <Grid item >
+                                <p style= {{fontWeight:"bold", fontSize:"18px"}}>Socials</p>
+                                <Stack spacing={2}>
+                                    <Instagram></Instagram>
+                                    <Facebook></Facebook>
+                                    <X></X>
+                                    <YouTube></YouTube>
+                                    
+                                </Stack>
+                            </Grid>
+                            
+                            
                         </Grid>
-                        <Grid item>
-                            <p style= {{fontWeight:"bold", fontSize:"18px"}}>Members</p>
-                            <Stack spacing={0}>
-                                <p style={{fontSize:"13px"}}>Member Events</p>
-                                <p style={{fontSize:"13px"}}>Our Team</p>
-                                <p style={{fontSize:"13px"}}>Highlights</p>
-                            </Stack>
-                        </Grid>
-                        <Grid item >
-                            <p style= {{fontWeight:"bold", fontSize:"18px"}}>Socials</p>
-                            <Stack spacing={2}>
-                                <img src={instagram} style={{width:"30px"}}></img>
-                                <img src={facebook} style={{width:"30px"}}></img>
-                                <img src={twitter} style={{width:"30px"}}></img>
-                            </Stack>
-                        </Grid>
-                        
-                        
                     </Grid>
-                </Grid>
-                
-            </Wrapper>
-        </Container>
+                    
+                    
+                </Wrapper>
+                <br></br>
+                <hr style={{width:"100%"}}></hr>
+                <p style={{fontSize:"13px"}}>&copy; {year} App Dev Club. All Rights Reserved</p>
+
+            </Container>
+            
+            
+
+
+            
         </div>
         
         
