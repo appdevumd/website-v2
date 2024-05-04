8a88aae0d8d9d73b6bc562edd932a8b08090c698
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2024-05-04 14:40:09 -0400

    removed unnecessary public in image src

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer(props: { sx?: SxProps }) {
     return (
         <Box sx={{ ...props.sx, display: 'flex', width: '100%', height: '75px', padding: '30px', alignItems: 'center', justifyContent: 'center' }}>
             <Typography style={{ fontSize: "18px", flexGrow: 1 }}>&copy; {year} App Dev Club</Typography>
-            <img src="../public/logo256.png" style={{ maxHeight: "50px" }} />
+            <img src="../logo256.png" style={{ maxHeight: "50px" }} />
             <Box sx={{ display: 'flex', gap: '10px', flexGrow: 1, justifyContent: 'end' }}>
                 <Instagram style={{ cursor: 'pointer', verticalAlign: "text-bottom", fontSize: "22px" }}></Instagram>
                 <LinkedIn style={{ cursor: 'pointer', verticalAlign: "text-bottom", fontSize: "22px" }}></LinkedIn>
