b7b1d61bda3b53db922c1449eba59f076fe91758
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2024-05-04 14:39:34 -0400

    added footer and navbar to every route

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,30 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import AppThemeController from './middleware/AppThemeController'
+import WebLandingPage from './integrals/WebLandingPage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import WebProjectsPage from './integrals/WebProjectsPage'
+import WebTierPage from './integrals/WebTierPage'
+import 'atropos/css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={AppThemeController.baseTheme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<WebLandingPage />} />
+          <Route path="/" element={<App />}>
+            <Route path="/project/:projectId" element={<WebProjectsPage />} />
+            <Route path="tier/:tier" element={<WebTierPage />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
+    </QueryClientProvider>
+  </BrowserRouter>
+);
